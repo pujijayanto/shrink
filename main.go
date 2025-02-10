d1e4756ec79c8236f30b24bbda20bd24027af718
Dedy <pujidjayanto@gmail.com>
2025-02-10 23:07:27 +0700

    feat: add header before write or will not have any effect

--- main.go
+++ main.go
@@ -6,14 +6,17 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Server", "Go")
 	w.Write([]byte("Shrink is an URL shortener"))
 }
 
 func shrink(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Server", "Go")
 	w.Write([]byte("Long URL become short"))
 }
 
 func redirectTo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Server", "Go")
 	w.Write([]byte("should redirect short URL to original URL"))
 }
 
