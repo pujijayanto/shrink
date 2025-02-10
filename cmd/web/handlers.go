bc11f0fe8979533987d99ad4b4f9e0cfb6fb0096
Dedy <pujidjayanto@gmail.com>
2025-02-11 06:15:57 +0700

    feat: introduce partials

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,14 +8,20 @@ import (
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl.html")
+	templateFiles := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/partials/nav.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
+	}
+
+	ts, err := template.ParseFiles(templateFiles...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "internal server error", http.StatusInternalServerError)
 		return
 	}
 
-	err = ts.Execute(w, nil)
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "internal server error", http.StatusInternalServerError)
