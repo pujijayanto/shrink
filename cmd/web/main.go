3c3610b4f5f7b1626b83ad98bfbc55156b5d7938
Dedy <pujidjayanto@gmail.com>
2025-02-11 22:21:33 +0700

    feat: introduce flag

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,52 +1,25 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
-	"path/filepath"
 )
 
 func main() {
+	addr := flag.String("addr", ":3002", "http network address")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./static")})
-
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("POST /{$}", shrink)
 	mux.HandleFunc("GET /{slug}", redirectTo)
 
-	log.Println("starting server on :3200")
-	err := http.ListenAndServe(":3200", mux)
+	log.Printf("starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
-}
-
-type neuteredFileSystem struct {
-	fs http.FileSystem
-}
-
-func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
-	f, err := nfs.fs.Open(path)
-	if err != nil {
-		return nil, err
-	}
-
-	s, err := f.Stat()
-	if err != nil {
-		return nil, err
-	}
-
-	if s.IsDir() {
-		index := filepath.Join(path, "index.html")
-		if _, err := nfs.fs.Open(index); err != nil {
-			closeErr := f.Close()
-			if closeErr != nil {
-				return nil, closeErr
-			}
-
-			return nil, err
-		}
-	}
-
-	return f, nil
 }
