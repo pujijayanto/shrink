26f08c51fc26394fd2f3a7cd5ac3ff3748b8b942
Dedy <pujidjayanto@gmail.com>
2025-02-11 22:37:48 +0700

    chore: inject structured log as dependency

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"log"
+	"log/slog"
 	"net/http"
 	"text/template"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 	templateFiles := []string{
 		"./ui/html/base.tmpl.html",
@@ -16,25 +16,27 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(templateFiles...)
 	if err != nil {
-		log.Println(err.Error())
+		app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
 		http.Error(w, "internal server error", http.StatusInternalServerError)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Println(err.Error())
+		app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
 		http.Error(w, "internal server error", http.StatusInternalServerError)
 		return
 	}
 }
 
-func shrink(w http.ResponseWriter, r *http.Request) {
+func (app *application) shrink(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
+	app.logger.Info("received request", slog.String("method", r.Method), slog.String("uri", r.URL.RequestURI()))
 	w.Write([]byte("Long URL become short"))
 }
 
-func redirectTo(w http.ResponseWriter, r *http.Request) {
+func (app *application) redirectTo(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
+	app.logger.Info("received request", slog.String("method", r.Method), slog.String("uri", r.URL.RequestURI()))
 	w.Write([]byte("should redirect short URL to original URL"))
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,23 +7,28 @@ import (
 	"os"
 )
 
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
 	addr := flag.String("addr", ":3002", "http network address")
 	flag.Parse()
 
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-		Level:     slog.LevelDebug,
-		AddSource: true,
+		Level: slog.LevelDebug,
 	}))
+
+	app := &application{logger: logger}
 
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("POST /{$}", shrink)
-	mux.HandleFunc("GET /{slug}", redirectTo)
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("POST /{$}", app.shrink)
+	mux.HandleFunc("GET /{slug}", app.redirectTo)
 
 	logger.Info("starting server", "addr", *addr)
 	err := http.ListenAndServe(*addr, mux)
